4e2feb0880f062cd7a355433010d1bc5aba0389f
Ann Jackson <ejackson227@ivytech.edu>
2026-03-21 22:18:59 -0400

    Initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Hi, my name is Emma, and I am a mobile developer</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
